4dd49940f9315cf2cb4265c098e7c1f24355f109
cmodog <53931809+cmodog@users.noreply.github.com>
2022-09-06 16:47:36 +0800

    Create openssh.sh

---
+++ openssh.sh
@@ -0,0 +1,3 @@
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl restart ssh
