88d05b68c4466e601d7c884e6122f0bb6e478d05
cmodog <53931809+cmodog@users.noreply.github.com>
2022-09-06 20:48:07 +0800

    Update .openssh.sh

--- .openssh.sh
+++ .openssh.sh
@@ -1,3 +1,4 @@
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo "        post-up iptables-restore < /etc/iptables" >> /etc/network/interfaces
 systemctl restart ssh
